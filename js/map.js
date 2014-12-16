81988695bcab7ce001d92be13b1a2ed99353b546
Ben Broadley <ben.broadley88@gmail.com>
2014-12-16 22:18:55 +0000

    update all

--- js/map.js
+++ js/map.js
@@ -1,12 +1,40 @@
 var map = (function(){
+	var canvas, ctx;
 	
-	console.log("hi");
+	var _init = function(){
+		//create the canvas element
+		_createCanvas();
+		
+		//prepare to respond to window resize
+		if(window.addEventListener){
+			window.addEventListener("resize",_resizeCanvas,false);
+		}else{
+			window.attachEvent("resize",_resizeCanvas);
+		}		
+		_draw();
+	}
+	var _createCanvas = function(){
+		canvas = document.createElement('canvas');
+		canvas.id = map;
+		_resizeCanvas();
+		document.getElementsByTagName("body")[0].appendChild(canvas);
+		ctx = canvas.getContext("2d");
+	}
+	var _resizeCanvas = function(){
+		canvas.width = window.innerWidth;
+		canvas.height = window.innerHeight;
+	}
+	var _draw = function(){
+		console.log("draw");
+	}
 	
 	
+	
+	
+	
+	//init just before the return to ensure all functions are correctly parsed
+	_init();
 	return {
 	
 	}
 })();
-
-
-map();
